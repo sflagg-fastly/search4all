c6cdc6c6d2b19de5dbab9e7fcbea5363e93a860a
Shaun Flagg <shaun.flagg@fastly.com>
2025-09-30 15:18:13 -0400

    fanout fixes

--- web/src/app/utils/fanout.ts
+++ web/src/app/utils/fanout.ts
@@ -14,23 +14,23 @@ export function connectFanout(
 ) {
   const es = new EventSource(`/stream/${channel}`, { withCredentials: false });
 
-  es.addEventListener('sources', (e: MessageEvent) => {
+  es.addEventListener("sources", (e: MessageEvent) => {
     onSources(JSON.parse(e.data));
   });
 
-  es.addEventListener('marker', (_e) => {
+  es.addEventListener("marker", (_e) => {
     // optional UI marker
   });
 
-  es.addEventListener('delta', (e) => {
-    onMarkdown(e.data ? JSON.parse(e.data).text : '');
+  es.addEventListener("delta", (e) => {
+    onMarkdown(e.data ? JSON.parse(e.data).text : "");
   });
 
-  es.addEventListener('related', (e) => {
+  es.addEventListener("related", (e) => {
     onRelates(JSON.parse(e.data));
   });
 
-  es.addEventListener('done', (_e) => es.close());
+  es.addEventListener("done", (_e) => es.close());
 
   es.onerror = (_e) => {
     es.close();
